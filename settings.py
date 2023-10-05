77f66819605a52e2a0397da8400e612ff7d4ef77
Gosiatomilo <contact@malgorzatatomilo.pl>
2023-10-05 17:46:24 +0200

    Ustawienia
    Dotyczące pocisków

--- settings.py
+++ settings.py
@@ -3,9 +3,13 @@ class Settings:
 
     def __init__(self):
         '''inicjalizacja ustawień gry'''
-
+# ustawienia dotyczące rakiety 
         self.screen_width = 1000
         self.screen_height = 600
         self.bg_color = (242,212,215)
         self.ship_speed = 1.5
-    
+# ustwienia dotyczące pocisków
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60,60,60)
