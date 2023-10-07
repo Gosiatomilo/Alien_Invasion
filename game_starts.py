ab118cf3699e30e73974343d0e0e483b3c8e76a3
Gosiatomilo <contact@malgorzatatomilo.pl>
2023-10-07 18:37:30 +0200

    Umieszczenie kominikat i wyśrodkowanie tekstu na przycisku

--- game_starts.py
+++ game_starts.py
@@ -6,7 +6,7 @@ class GameStats:
         self.settings = ai_game.settings
         self.reset_stats()
         # Uruchomienie gry "Inwazja obcych" w stanie aktywnym
-        self.game_active = True
+        self.game_active = False 
 
     def reset_stats(self):
         '''Inicjalizacja danych statystycznych, które mogą zmieniać się w trakcie gry'''
