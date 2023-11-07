e9aa828b27231aacaa1d26c0976f35afdfcb99cb
Gosia <contact@malgorzatatomilo.pl>
2023-11-07 19:18:31 +0100

    Change of comments to English

--- settings.py
+++ settings.py
@@ -1,29 +1,35 @@
 class Settings:
-    '''klasa przeznaczona do przechowywania wszystkich ustawiań gry'''
+    """A class designed to hold all game settings"""
 
     def __init__(self):
-        '''inicjalizacja ustawień gry'''
-# ustawienia dotyczące rakiety 
+        """Initialise game settings"""
+        # Rocket settings
         self.screen_width = 1000
-        self.screen_height = 600
-        self.bg_color = (242,212,215)
-# ustawienia dotyczące ststk kosmicznego 
+        self.screen_height = 800
+        self.bg_color = (242, 212, 215)
+
+        # Spacecraft settings
         self.ship_limit = 3
-# ustwienia dotyczące pocisków
+
+        # Arrangements for missiles
         self.bullet_width = 3
         self.bullet_height = 15
-        self.bullet_color = (60,60,60)
+        self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 3
-# ustawienia dotyczące kosmitów
+
+        # Alien settings
         self.fleet_drop_speed = 10
-# Łatwa zmiana szybkości gry
-        self.speedup_scale = 1.1 
-        # Łatwa zmiana liczby punktów przyznawanych za zastrzelenie obcego
+
+        # Easy change of game speed
+        self.speedup_scale = 1.1
+
+        # Easy change to the number of points awarded for shooting a stranger
         self.score_scale = 1.5
+
         self.initialize_dynamic_settings()
 
     def initialize_dynamic_settings(self):
-        '''Inicjalizacja ustawień, które ulegają zmianie w trakcie gry'''
+        """Initialisation of settings that change during the game"""
         self.ship_speed = 1.5
         self.bullet_speed = 3.0
         self.alien_speed = 1.0
@@ -32,9 +38,9 @@ class Settings:
         self.alien_points = 50
 
     def increase_speed(self):
-        '''Zmiana ustawień dotyczących szybkości'''
+        """Changing the speed settings"""
         self.ship_speed *= self.speedup_scale
         self.bullet_speed *= self.speedup_scale
         self.alien_speed *= self.speedup_scale
+
         self.alien_points = int(self.alien_points * self.score_scale)
-        print(self.alien_points)
