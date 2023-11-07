2de5683f3b5a9b08b7563090ba6c083b61dc4b1d
Gosia <contact@malgorzatatomilo.pl>
2023-11-07 18:40:38 +0100

    Change of comments to English

--- bullet.py
+++ bullet.py
@@ -1,29 +1,30 @@
 import pygame
 from pygame.sprite import Sprite
 
-class Bullet(Sprite):
-    '''Klasa przeznaczona do zarządzania pociskami wystrzeliwanymi przez statek'''
 
+class Bullet(Sprite):
+    """A class designed to manage the missiles fired by a ship"""
     def __init__(self, ai_game):
-        '''Utworzenie obiektu pocisku w aktualnym połozeniu statku'''
+        """Creation of a projectile object in the current position of the ship"""
         super().__init__()
         self.screen = ai_game.screen
         self.settings = ai_game.settings 
         self.color = self.settings.bullet_color
 
-# Utworzenie prostokąta pocisku w punkcie (0,0), a następnie zdefiniowania dla niego odpowiedniego połozenia
+        # Create a projectile rectangle at point (0,0) and then define a suitable position for it
         self.rect = pygame.Rect(0, 0, self.settings.bullet_width, self.settings.bullet_height) 
         self.rect.midtop = ai_game.ship.rect.midtop
-# Połozenie pocisku 
+
+        # Store the bullet's position as a decimal value
         self.y = float(self.rect.y)
 
     def update(self):
-        '''Poruszanie pociskiem po ekranie'''
-# Uaktualnianie połozenia pocisku 
-        self.y -=self.settings.bullet_speed
-# Uaktualnianie połozenia prostokąta pocisku
+        """Moving a bullet across the screen"""
+        # Updating the bullet position
+        self.y -= self.settings.bullet_speed
+        # Updating the position of the projectile rectangle
         self.rect.y = self.y
 
     def draw_bullet(self):
-        '''Wyświetlenie pocisku na ekranie'''
+        """Displaying the projectile on the screen"""
         pygame.draw.rect(self.screen, self.color, self.rect)
