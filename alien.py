4180c9962a3f6787b5d18f3014740635e8bdd028
Gosia <contact@malgorzatatomilo.pl>
2023-11-07 16:57:50 +0100

    Changing comments to English and optimising code

--- alien.py
+++ alien.py
@@ -2,35 +2,31 @@ import pygame
 from pygame.sprite import Sprite
 
 class Alien(Sprite):
-    '''Klasa przedstawiająca pojedyńczego obcego i zdefiniowanie jego połozenia początkowego'''
+    '''Class representing a single alien and defining its initial position'''
     def __init__(self, ai_game):
+        '''Initialize the alien and set its starting position.'''
         super().__init__()
         self.screen = ai_game.screen
         self.settings = ai_game.settings
-        # Wczytanie obrazu obcego i zdefiniowanie atrybutu rect 
+
+        # Loading a foreign image and defining the rect attribute
         self.image = pygame.image.load('/Users/malgorzatatomilo/Documents/GitHub/InwazjaObcych/ufo-4778062_640.bmp')
         self.rect = self.image.get_rect()
-        # Umieszczenie nowego obcego w poblizu lewego górnego rogu ekranu
+
+        # Placing a new alien near the top left corner of the screen
         self.rect.x = self.rect.width
         self.rect.y = self.rect.height
-        # Przechowywanie dokładnego poziomego połozenia obcego 
+
+        # Storing the exact horizontal position of the alien
         self.x = float(self.rect.x)
 
-    def update(self):
-        '''Przeunięcie obcego w prawo'''
-        self.x += self.settings.alien_speed
-        self.rect.x = self.x
-
-
     def check_edges(self):
-        '''Zwraca wartość True, jeśli obcy znajduję się przy krawędzi ekranu'''
+        '''Returns True if the alien is at the edge of the screen'''
         screen_rect = self.screen.get_rect()
         if self.rect.right >= screen_rect.right or self.rect.left <= 0:
             return True
     
     def update(self):
-        '''Przesunięcie obcego w prawo lub lewo'''
+        '''Moving an alien to the right or left'''
         self.x += (self.settings.alien_speed * self.settings.fleet_direction)
-        
         self.rect.x = self.x
-
